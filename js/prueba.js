edf0bff8cac890b311aa4db10e620eb6b7d7c702
angoreno <isaac.pizarroc2@gmail.com>
2020-06-11 08:19:37 -0400

    actualizacion de JS

--- js/prueba.js
+++ js/prueba.js
@@ -1,4 +1,15 @@
-var done=0; 
+var intento=0; 
+var userName="";
+
+$('#login').click(function(){
+    var user = $('#usuario').val();
+    var pass = $('#pass').val();
+
+    var uName=login(user,pass);
+
+    $('#nombreUsuario').val(uName);
+
+})
 
 /**
  * 
@@ -10,25 +21,39 @@ function login(user,pass){
     
     if (user=="nmora" && pass=="123456") { 
         window.location="index.html"; 
+        userName="Natalia Mora";
     } 
     if (user=="cpena" && pass=="123456") { 
-        window.location="index.html"; 
+        window.location="index.html";
+        userName="consuelo Peña"; 
     } 
     if (user=="ipizarro" && pass=="123456") { 
         window.location="index.html"; 
+        userName="Isaac Pizarro";
         } 
     if (user=="" && pass=="") { 
         window.location="errorpopup.html"; 
-        done++;
+        userName="";
+        intento++;
     } else{
         window.location="errorpopup.html";
-        done++;
+        userName="";
+        intento++;
     }
+    return userName;
 }
 
 function revConexion(){
 
-    //funcion para revisar si esta conectado y cambiar el boton a cerrar sesion o Login
+    if(userName=="Natalia Mora"||
+        userName=="Consuelo Peña"||
+        userName=="Isaac Pizarro"){
+        //label superior izquierda aparecera nombre
+        //de uusario que esta conectado.
+
+    }else{
+        //label aparecera como anonimo
+    }
 
 }
 
