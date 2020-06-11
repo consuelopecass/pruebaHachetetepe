86492a3dc4f36d273ed636b7832cfa029be53b0a
angoreno <isaac.pizarroc2@gmail.com>
2020-06-10 20:05:51 -0400

    escribe funcion login

--- prueba.js
+++ prueba.js
@@ -2,8 +2,25 @@
  * 
  * @param {usuario} user 
  * @param {contraseña} pass 
+ * Funcion para realizar login en la pagina web.
  */
 function login(user,pass){
+
+    var done=0; 
+    
+    if (user=="nmora" && pass=="123456") { 
+    window.location="TU_PAGINA_WEB.HTML"; 
+    } 
+    if (user=="cpena" && pass=="123456") { 
+    window.location="TU_PAGINA_WEB.HTML"; 
+    } 
+    if (user=="ipizarro" && pass=="123456") { 
+        window.location="TU_PAGINA_WEB.HTML"; 
+        } 
+    if (user=="" && pass=="") { 
+    window.location="errorpopup.html"; 
+    } 
+ 
 
 
 
